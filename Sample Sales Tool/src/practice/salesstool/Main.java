47fe1daaf3c6c498c3c14c2e35243a8c9b8ea2fa
Saritha <Saritha@10.0.0.250>
2016-06-24 07:40:04 -0400

    Inital checking of Sample Sales

---
+++ Sample Sales Tool/src/practice/salesstool/Main.java
@@ -0,0 +1,18 @@
+package practice.salesstool;
+
+public class Main {
+
+	public static void main(String[] args)
+	{	samplesales data=new samplesales();
+		data.display();
+		reflection();
+	}
+		public static void reflection()
+		{
+			System.out.println("THIS IS AN EXAMPLE");
+			System.out.println("HELLO WORLD");
+		}
+
+	
+
+}
---
+++ Sample Sales Tool/src/practice/salesstool/samplesales.java
@@ -0,0 +1,12 @@
+package practice.salesstool;
+
+public class samplesales 
+{
+	int data[]={0,23,45};
+
+	public void display()
+	{
+		System.out.println("I am a beginner");
+	}
+
+}
